73dd1ef4d3e3a8e578e6f153bd0bae1b1c867db7
Bianca <90906553+BibiChr@users.noreply.github.com>
2023-06-03 20:08:12 +0200

    week 11

--- woche1/admin-userCreateAndRights.sql
+++ woche1/admin-userCreateAndRights.sql
@@ -26,3 +26,21 @@ commit;
 -- delete user
 drop user bianca; -- problem if user has objects
 drop user bianca cascade; -- delete with objects
+-- show user welche manuell angelegt wurden
+SELECT username, account_status, default_tablespace, created
+ FROM dba_users
+ WHERE oracle_maintained = 'N';
+
+-- Haben alle User die Privilegien CREATE SESSION und CREATE TABLE?
+SELECT * FROM dba_sys_privs
+ WHERE grantee IN (
+    SELECT username FROM dba_users
+      WHERE oracle_maintained = 'N' AND username != 'PDBADMIN')
+ORDER BY grantee, privilege;
+-- Sind für jeden User mindestens die Tabellen EMP und DEPT vorhanden?
+SELECT owner, table_name, tablespace_name FROM dba_tables
+ WHERE owner IN (
+   SELECT username FROM dba_users
+      WHERE oracle_maintained = 'N' AND username != 'PDBADMIN')
+ORDER BY owner, table_name;
+
